741811b5d7a53a2ff58da37caa87a6fd51d847a6
Adrian Dusza <adix118@gmail.com>
2017-04-12 11:30:23 +0200

    Revert "update"
    This reverts commit dce14938bb62e07d27b6da38f48d88cce96e454f.

--- bin/contact_me.php
+++ bin/contact_me.php
@@ -16,11 +16,11 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'stalfach@vp.pl'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Formularz kontaktowy na stronie Exclusive:  $name";
-$email_body = "Otrzymałeś nową wiadomość z formularza kontaktowego na stronie.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nTelefon: $phone\n\nWiadomość:\n$message";
-$headers = "Od: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Odpowiedz do: $email_address";   
+$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
 ?>
